3a0f7a54275d1391c7709c0d11845d788d3b5541
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-09-22 16:35:06 +0200

    feat: add jest && enzyme

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { defaults } = require('jest-config')
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  setupTestFrameworkScriptFile: './src/test/setupTest.ts',
+  unmockedModulePathPatterns: ['react', 'enzyme', 'jest-enzyme'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'tsconfig.spec.json',
+    },
+  },
+}
