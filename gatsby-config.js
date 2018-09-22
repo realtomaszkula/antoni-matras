2e09fcd5131ab01e7b730f623cb60018813bb82d
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-09-22 15:19:01 +0200

    feat: add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,12 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-helmet', {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
